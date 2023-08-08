e74437f491399428c96eb902d8119336a5cce096
ManeeshaManee <129473802+ManeeshaManee@users.noreply.github.com>
2023-08-08 16:20:26 +0530

    Update forms.py

--- bankproject/bankapp/forms.py
+++ bankproject/bankapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Booking
+from .models import Booking, City
 
 
 class DateInput(forms.DateInput):
@@ -14,6 +14,8 @@ class BookingForm(forms.ModelForm):
         widgets = {
             'booking_date': DateInput(),
             'date_birth': DateInput(),
+            'Gender': forms.RadioSelect(),
+
         }
         labels = {
             'date_birth': "DATE OF BIRTH",
@@ -23,9 +25,27 @@ class BookingForm(forms.ModelForm):
             'u_phone': "CONTACT NUMBER",
             'u_email': "EMAIL ADDRESS",
             'u_address': "HOME ADDRESS",
-            'dic_name': "DISTRICT",
-            'branch_name': 'BRANCH',
+            # 'dic_name': "DISTRICT",
+            # 'branch_name': 'BRANCH',
+            'country': "DISTRICT",
+            'city': 'BRANCH',
             'ac_type': 'ACCOUNT TYPE',
             'booking_date': 'DATE OF BOOKING',
 
+
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['city'].queryset = City.objects.none()
+
+        if 'country' in self.data:
+            try:
+                country_id = int(self.data.get('country'))
+                self.fields['city'].queryset = City.objects.filter(country_id=country_id).order_by('name')
+            except (ValueError, TypeError):
+                pass  # invalid input from the client; ignore and fallback to empty City queryset
+        elif self.instance.pk:
+            self.fields['city'].queryset = self.instance.country.city_set.order_by('name')
+
+
