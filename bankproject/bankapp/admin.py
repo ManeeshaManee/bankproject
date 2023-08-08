a6700202417301c89a7b3bd4f940e644517fe667
ManeeshaManee <129473802+ManeeshaManee@users.noreply.github.com>
2023-08-08 16:20:51 +0530

    Update admin.py

--- bankproject/bankapp/admin.py
+++ bankproject/bankapp/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
-from .models import Departments,District,Branchname,Account_type,Booking
+from .models import Departments,Account_type, Booking, Country, City
 
 # Register your models here.
 admin.site.register(Departments)
 admin.site.register(Account_type)
-admin.site.register(District)
-admin.site.register(Branchname)
+# admin.site.register(District)
+# admin.site.register(Branchname)
+admin.site.register(Country)
+admin.site.register(City)
+
+
+
 
 
 class BookingAdm(admin.ModelAdmin):
-    list_display = ('id', 'u_name','date_birth', 'u_phone', 'u_email','u_address','dic_name','branch_name','ac_type','booking_date','booked_on','DEBIT_CARD','CREDIT_CARD','CHEQUE_BOOK')
+    list_display = ('id', 'u_name','date_birth', 'u_phone', 'u_email','u_address','city','country','ac_type','booking_date','booked_on','DEBIT_CARD','CREDIT_CARD','CHEQUE_BOOK')
 
 
 admin.site.register(Booking, BookingAdm)
