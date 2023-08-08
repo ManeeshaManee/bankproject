fb7b9174235b42421ce8162c4b4579f6788f605f
ManeeshaManee <129473802+ManeeshaManee@users.noreply.github.com>
2023-08-08 16:19:10 +0530

    Update urls.py

--- bankproject/bankapp/urls.py
+++ bankproject/bankapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('branches/', views.branches, name="branches"),
     path('booking/', views.booking, name="booking"),
     path('department/', views.department, name="departments"),
+    path('<int:pk>/', views.person_update_view, name='person_change'),
+    path('ajax/load-cities/', views.load_cities, name='ajax_load_cities'),  # AJAX
 ]
